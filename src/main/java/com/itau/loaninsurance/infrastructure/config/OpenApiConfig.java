5336255f87a2ea4be88b8035e45520b47a5de674
eduardo <eduardofrad.dev@gmail.com>
2025-11-23 17:56:45 -0300

    feat(openapi): habilitar botão Authorize adicionando bearerAuth ao Swagger

--- src/main/java/com/itau/loaninsurance/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/itau/loaninsurance/infrastructure/config/OpenApiConfig.java
@@ -1,15 +1,34 @@
 package com.itau.loaninsurance.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public OpenAPI api() {
-        return new OpenAPI().info(new Info().title("Quotes API").version("v1").description("Cotação de seguro prestamista"));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Quotes API")
+                        .version("v1")
+                        .description("Cotação de seguro prestamista")
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
-
